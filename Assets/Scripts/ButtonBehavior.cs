1981a85ee4aa969e4fc85ff5977d097c33c22f3d
Michael Santos <mjs8837@rit.edu>
2022-09-17 13:57:21 -0400

    Got the basics of the main menu scene working. Could use some styling changes in the future.

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class ButtonBehavior : MonoBehaviour
 {
+    MeshRenderer mersh;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mersh = gameObject.GetComponent<MeshRenderer>();
+        mersh.material.color = Color.black;
     }
 
     // Update is called once per frame
@@ -16,8 +21,29 @@ public class ButtonBehavior : MonoBehaviour
         
     }
 
+    private void OnMouseEnter()
+    {
+        mersh.material.color = Color.grey;
+        gameObject.transform.localScale = gameObject.transform.localScale * 1.1f;
+    }
+
+    private void OnMouseExit()
+    {
+        mersh.material.color = Color.black;
+        gameObject.transform.localScale = gameObject.transform.localScale * 0.9f;
+    }
+
     private void OnMouseDown()
     {
-        Debug.Log("TEST");
+        if (gameObject.name == "Start Button")
+        {
+            SceneManager.LoadScene("PreMaze");
+        }
+
+        if (gameObject.name == "Exit Button")
+        {
+            EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
     }
 }
