e3747b674ba5544c14a165d6184ebc009ac3ca7e
mjs8837 <mjs8837@rit.edu>
2022-10-13 18:02:28 -0400

    Almost finished up the distance tracker script.

--- Assets/Scripts/DistanceTracker.cs
+++ Assets/Scripts/DistanceTracker.cs
@@ -5,27 +5,88 @@ using UnityEngine.SceneManagement;
 
 public class DistanceTracker : MonoBehaviour
 {
-    [SerializeField] GameObject infinadeck;
-    float runTimer;
+    [SerializeField] private GameObject infinadeck;
+    private InfinadeckLocomotion locomotion;
+    private float runTimer = 10.0f;
+    private Vector3 distance;
+    private float maxDistance = 10.0f;
+    private float locomotionConstraint = 0.00001f;
+
+    private bool isStarted, useTime = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        runTimer = 10.0f;
+        locomotion = infinadeck.GetComponent<InfinadeckCore>().locomotion.GetComponent<InfinadeckLocomotion>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (runTimer > 0.0f)
-        {
-            runTimer -= Time.deltaTime;
-        }
+        if (Mathf.Abs(locomotion.xDistance) > locomotionConstraint || Mathf.Abs(locomotion.yDistance) > locomotionConstraint) { isStarted = true; }
 
-        if (runTimer <= 0.0f)
+        TimeOrDistance();
+
+        HandleSceneChange();
+    }
+
+    // Helper function to calculate total distance traveled
+    private float UpdateDistance()
+    {
+        // Getting the distance from the locomotion of the treadmill
+        distance.x += Mathf.Abs(locomotion.xDistance);
+        distance.z += Mathf.Abs(locomotion.yDistance);
+        float totalDistance = distance.magnitude;
+
+        return totalDistance;
+    }
+
+    private void HandleSceneChange()
+    {
+        if (isStarted)
+        {
+            // Checking if the user has traveled a certain distance
+            if (!useTime)
+            {
+                if (UpdateDistance() >= maxDistance) // Fix magic number
+                {
+                    SceneTransition();
+                }
+            }
+            else
+            {
+                if (runTimer > 0.0f)
+                {
+                    runTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    SceneTransition();
+                }
+            }
+        }
+    }
+
+    private void TimeOrDistance()
+    {
+        if (!isStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                useTime = !useTime;
+            }
+        }
+    }
+
+    private void SceneTransition()
+    {
+        // Set up an indicator for the player to stop moving
+        Debug.Log("Stop moving");
+
+        // Handle logic to check if the user stopped moving and transport them to the maze
+        if (Mathf.Abs(locomotion.xDistance) < locomotionConstraint && Mathf.Abs(locomotion.yDistance) < locomotionConstraint)
         {
             SceneManager.LoadScene("MazeProtocol");
         }
-        //Debug.Log(infinadeck.GetComponent<InfinadeckCore>().locomotion.GetComponent<InfinadeckLocomotion>().xDistance);
     }
 }
